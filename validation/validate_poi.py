9c55ed1c3e0c2cb9629d8a0cdb6637f2c076d6e5
Cid <46410491+cidmedeiros@users.noreply.github.com>
2019-02-11 21:11:11 -0200

    Started building pipeline and validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+ #!/usr/bin/python
 
 
 """
@@ -14,19 +14,31 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, label_train, label_test = train_test_split(features, labels, test_size = 0.3, random_state = 42) 
 
+### it's all yours from here forward!
 
-### it's all yours from here forward!  
+param = {max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, class_weight=None, presort=False}
 
+clf = tree.DecisionTreeClassifier() 
+
+clf.fit(features_train, label_train)
+
+pred = clf.predict(features_test)
+
+print(accuracy_score(pred, label_test))
 
